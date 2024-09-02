eb3099ef31bfd72298016c7a47b7f9096a6ec9d7
Rushabh.shah <rushabh.shah@gupshup.io>
2024-09-03 02:28:44 +0530

    client setup v1.0

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CreateUser from "./Component/URL-create/CreateUser";
+function App() {
+  return (
+    <div className="App">
+      <CreateUser>
+      <h1>Create User</h1>
+      </CreateUser>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Component/URL-create/CreateUser.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import axios from "axios";
+const CreateUser = () => {
+  const [shortUrl, setshortUrl] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const url = event.target.url.value;
+    console.log("Form submitted", url);
+    axios
+      .post("http://localhost:8001/url", { url })
+      .then((response) => {
+        console.log(response.data.id);
+        setshortUrl("http://localhost:8001/"+response.data.id, []);
+        console.log(shortUrl);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div>
+      <h1>Create User</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Enter Url to short:</label>
+        <br></br>
+        <input type="text" name="url" required />
+        <br></br>
+        <button>Submit</button>
+        <div>{!shortUrl?"":
+          <label>Short URL is:- <a href={shortUrl}>{shortUrl}</a></label>
+        }
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateUser;
